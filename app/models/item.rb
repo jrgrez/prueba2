ad68eec84b0220f4ce9d144a4334fbd194632e22
Joaquin Grez <joaquingrez@gmail.com>
2017-07-19 20:20:31 -0400

    Sexto Commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :category
-  belongs_to :user, optional: true
+  belongs_to :user, optional: true, dependent: :destroy
+
+  after_destroy :erase_last
 
   scope :more_than, ->(size_searched){where("size >= ?", size_searched)}
   scope :no_user, ->{where(user_id: nil)}
+
+  private
+  def erase_last
+ 	self.category.destroy if self.category.items.count == 0
+  end
 end
