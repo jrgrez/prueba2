8b7282ab5ab246e925c519989ff49f766d72401d
Joaquin Grez <joaquingrez@gmail.com>
2017-07-19 20:17:19 -0400

    Quinto Commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
+
+  scope :more_than, ->(size_searched){where("size >= ?", size_searched)}
+  scope :no_user, ->{where(user_id: nil)}
 end
